3213b0883de3e50b4bc28b5703a5904e552cde63
chrixob <chrisobodai40@gmail.com>
2024-09-06 10:57:18 +0000

    prompt user to select lesson on initial load

--- src/components/WorkingArea.js
+++ src/components/WorkingArea.js
@@ -9,11 +9,13 @@ const WorkingArea = ({ activeLesson, content, title, goToNextLesson}) => {
   console.log("Selected lesson: " + activeLesson)
 
   return (
-    <div className="container d-flex flex-column align-items-center justify-content-center align-text-left" style={{ marginTop: '13%' }}>
+    <div className="container d-flex flex-column align-items-center justify-content-center align-text-left" 
+    style={{ marginTop: '13%' }}>
       {title && <h2 className="mb-4">{title}</h2>}
-      <div className="content-area p-4" style={{ backgroundColor: '#e0f7fa', borderRadius: '8px' }}>
+      <div className="content-area p-4" style={{ backgroundColor: '#e0f7fa', borderRadius: '8px', paddingLeft:'20px' }}>
         {content}
         {isLesson && <IframeEditor />}
+        {!activeLesson && <p><h3>Please click on the icon at the top left corner and select a lesson to continue</h3></p>}
       </div>
     </div>
   );
